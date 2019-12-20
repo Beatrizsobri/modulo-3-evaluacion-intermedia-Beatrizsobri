61332a7d53038a3cdfdf85d21ea7eca7048e7780
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-12-20 08:41:58 +0100

    Update docs

---
+++ docs/precache-manifest.4a4eb635787fc03bd2fb4b7516878a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefba5d5b92e2ac00621082d0e105628",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7ee8beaabea3f862411",
+    "url": "./static/css/main.853b68c4.chunk.css"
+  },
+  {
+    "revision": "59dce7e7a81256ddf771",
+    "url": "./static/js/2.6b699b3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b699b3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7ee8beaabea3f862411",
+    "url": "./static/js/main.f2633a94.chunk.js"
+  },
+  {
+    "revision": "a9eb3b777736d22f046d",
+    "url": "./static/js/runtime-main.e99222d3.js"
+  }
+]);
