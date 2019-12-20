0fc32082cdffd50166927af8e833c426da3362b1
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-12-20 09:38:39 +0100

    Add types

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import ApiData from '../data/Api';
-import Pokemontypes from './Pokemontypes'
+import Pokemontypes from './Pokemontypes';
 
-
-
-
-class Pokemon extends React.Component {
-    render(){
-    return (
+function Pokemon (props) {
+    let types = props.types
+    return(
     <div>
-        <img alt={this.props.name} src={this.props.src}/>
-        <h2>{this.props.name}</h2>
-    <ul>
-        {/* {ApiData.map((Api,key)=>{})} */}
-        <Pokemontypes types={ApiData[0].types}/>
-    </ul>
-</div>
+        <img alt={props.name} src={props.src}/>
+        <h2>{props.name}</h2>
+        <ul>
+        {types.map((type,key)=>{
+            return <Pokemontypes types={type} key={key}/>
+        })}
+        </ul>
+    </div>
 
-    );
-    }
+    )
 }
+
 
 export default Pokemon;
